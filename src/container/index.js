9e3a39b9a6bab597da3f97d0e582e7a8dca2563d
Abhishek Mishra <abhishek@digiqt>
2021-03-20 11:26:10 +0530

    INIT

--- src/container/index.js
+++ src/container/index.js
@@ -1,7 +1,27 @@
 import React from "react";
+import axios from "axios";
 
 const HomePage = () => {
-  return <div>React Boilerplate by DigiQT Technolabs</div>;
+  const [apiState, setAPIState] = React.useState("Initial API State");
+
+  const makeAPICall = () => {
+    console.log("API Call");
+  };
+  return (
+    <div className="row">
+      <div className="col-4" />
+      <div className="col-md-3">
+        <button
+          onClick={() => makeAPICall()}
+          className="btn btn-outline-primary ml-4"
+        >
+          Make an API call here
+        </button>
+        <p className="ml-4 mt-2">{apiState}</p>
+      </div>
+      <div className="col-4" />
+    </div>
+  );
 };
 
 export default HomePage;
